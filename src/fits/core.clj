933d05a394a0ee3b6294545e193a5831d2228864
Bobby Harris <bobby.harris@gmail.com>
2018-03-07 06:55:35 -0500

    broken

--- src/fits/core.clj
+++ src/fits/core.clj
@@ -1,5 +1,19 @@
 (ns fits.core
+  (:require [clojure.java.io :refer :all])
   (:gen-class))
+(def file-name "resources/180305_0000001884.fits")
+
+(let [f (file file-name )]
+  (with-open [in (input-stream f)]
+   (let [size (.length f)
+         buf (byte-array size)
+         _ (.read in buf)]
+     (println "Read" size "bytes.")
+    (with-open [out (output-stream (file "test.fits"))]
+      (.write out (subvec buf 0 1123))
+      (.write out "_OBS")
+      (.write out (subvec buf 1127 size)) ))))
+
 
 (defn edit-all-files-in-directory [dir]
   (let [d (clojure.java.io/file dir)
@@ -12,5 +26,6 @@
         (spit (str "edited-" (.getName f)) edited )))))
 
 (defn -main
+  "I don't do a whole lot ... yet."
   [& args]
   (edit-all-files-in-directory (first args)))
